4243d95cc1904c585d8fc2b7dce4e096e8c4be76
RosexWolf <jreinasolano@gmail.com>
2022-02-21 10:51:11 -0500

    .

--- aritmetica.py
+++ aritmetica.py
@@ -1,3 +1,5 @@
+import math
+
 def sumDosNum(num1,num2):
     return num1+num2
 
@@ -9,3 +11,13 @@ def multDosNum(num1,num2):
 
 def divDosNum(num1,num2):
     return num1/num2
+
+def NumPI(numPI):
+    return math.pi
+
+def NumPHI(numPHI):
+    return 1.16e-4
+
+def NumGrav(numGrav):
+    return 6.67e-11
+ 
