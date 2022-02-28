e36fd6b43f15a117366b4e09ef428ae1bb975d49
RosexWolf <jreinasolano@gmail.com>
2022-02-28 09:34:47 -0500

    Tercera Actualizacion

--- index.py
+++ index.py
@@ -1,17 +1,46 @@
+from traceback import print_tb
 import aritmetica as t
 import text as a
 
 print(a.Title("Este ","programa ","realiza ","operaciones ","matematicas."))
 
+print("1.Suma")
+print("2.Resta")
+print("3.Multiplicacion")
+print("4.Division")
+print("5.Numero PI")
+print("6.Numero PHI o Aureo")
+print("7.Costante Gravitacional")
 
-print(a.concat("Suma ","de ","Numeros"))
-print(t.sumDosNum(2,3))
+opc=int(input("Escoge un numero: "))
+if opc == 1:
+   print(a.concat("Suma ","de ","Numeros"))
+   print(t.sumDosNum(2,3))
 
-print(a.concat1("Resta ","de ","Numeros"))
-print(t.resDosNum(2,3))
+elif opc == 2:
 
-print(a.concat2("Multiplicacion ","de ","Numeros"))
-print(t.multDosNum(2,3))
+    print(a.concat1("Resta ","de ","Numeros"))
+    print(t.resDosNum(2,3))
 
-print(a.concat3("Division ","de ","Numeros"))
-print(t.divDosNum(2,3))
+elif opc == 3:
+
+    print(a.concat2("Multiplicacion ","de ","Numeros"))
+    print(t.multDosNum(2,3))
+
+elif opc == 4:
+
+   print(a.concat3("Division ","de ","Numeros"))
+   print(t.divDosNum(2,3))
+
+elif opc == 5:
+   print(a.concat4("Este ","el "," Numero"," Pi"))
+   print(t.NumPI)
+elif opc == 6:
+    print(a.concat5("Este "," es "," el"," Numero"," Aureo"))
+    print(t.NumPHI("--"))
+elif opc == 7:
+    print(a.concat6("Esta "," es "," la"," constante"," de"," Gravedad"))
+    print(t.NumGrav("--"))
+else:
+    print("Opcion no valida")
+
--- text.py
+++ text.py
@@ -15,3 +15,15 @@ def concat2(text1,text2,text3):
 
 def concat3(text1,text2,text3):
     return text1+text2+text3
+
+def concat4 ( texto1 , texto2 , texto3 , texto4 ):
+    return  texto1 + texto2 + texto3 + texto4
+    
+def concat5 ( texto1 , texto2 , texto3 , texto4 , texto5 ):
+    return texto1 + texto2 + texto3 + texto4 + texto5
+
+def concat6 ( texto1 , texto2 , texto3 , texto4 , texto5 , texto6 ):
+    return  texto1 + texto2 + texto3 + texto4 + texto5 + texto6
+    
+def concat7 ( texto1 , texto2 , texto3 , texto4 , texto5 ):
+    return texto1 + texto2 + texto3 + texto4 + texto5
